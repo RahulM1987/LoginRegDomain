94cfedbb70e39fb9576341523cee0f198f7b0aef
Rahul <rahul.love1234@gmail.com>
2018-08-06 00:11:12 +0530

    Initial commit

---
+++ LoginRegisterDomain/RegisterLogin.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterLogin.swift
+//  LoginRegisterDomain
+//
+//  Created by Rahul's MacBook Pro on 04/08/18.
+//  Copyright © 2018 Rahul M. All rights reserved.
+//
+
+import Foundation
---
+++ LoginRegisterDomain/ScanQR.swift
@@ -0,0 +1,35 @@
+//
+//  ScanQR.swift
+//  LoginRegisterDomain
+//
+//  Created by Rahul's MacBook Pro on 05/08/18.
+//  Copyright © 2018 Rahul M. All rights reserved.
+//
+
+import UIKit
+
+class ScanQR: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
